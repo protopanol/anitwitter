9a1fac5e461a314e71c5cdfb60a0788bd305ba1e
Protopanol <protopanol@gmail.com>
2014-11-01 22:25:19 -0300

    Body parser middleware, new routes, modal form fixes.

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var serveStatic = require('serve-static');
 var config = require('./config');
 var session = require('cookie-session');
 var csrf = require('csurf');
+var bodyParser = require('body-parser');
 
 app.use(serveStatic('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 app.use(session({
   keys: config.session.keys,
 }))
@@ -18,7 +24,15 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', { csrfToken: req.csrfToken() });
+});
+
+app.post('/login', function (req, res) {
+  res.render('index', { csrfToken: req.csrfToken() });
+});
+
+app.post('/register', function (req, res) {
+  res.render('index', { csrfToken: req.csrfToken() });
 });
 
 app.use(function (err, req, res, next) {
